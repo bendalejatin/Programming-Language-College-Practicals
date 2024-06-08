4f420c2da5c93f8045bdaa9b419cdd31febed715
Jatin Bendale <166155605+bendalejatin@users.noreply.github.com>
2024-06-08 19:37:10 +0530

    prc1 uploaded

---
+++ practical1.c
@@ -0,0 +1,17 @@
+//Practical : 1- Write a C program to display printf function:
+//Enrollment No: 202203103510038
+//Name : Jatin Bendale
+//Branch : B.Tech. Computer Science and Engineering
+
+#include<stdio.h>
+void main()
+{
+    printf("\n###################################################\n"); 
+
+    printf("Student Name: Jatin Bendale \n");
+    printf("Branch Name: B.Tech Computer Science and Engineering \n");
+    printf("Enrollment Number: 202203103510038 \n");
+
+    printf("\n###################################################\n"); 
+
+}
