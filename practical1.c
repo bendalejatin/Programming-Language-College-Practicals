5d21e944c278b3950a7ac8d9443b68c342309fb2
Jatin Bendale <166155605+bendalejatin@users.noreply.github.com>
2024-08-17 16:30:28 +0530

    Update practical1.c

--- practical1.c
+++ practical1.c
@@ -1,7 +1,4 @@
 //Practical : 1- Write a C program to display printf function:
-//Enrollment No: 202203103510038
-//Name : Jatin Bendale
-//Branch : B.Tech. Computer Science and Engineering
 
 #include<stdio.h>
 void main()
