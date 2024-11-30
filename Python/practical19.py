58ca5b8b07b8350e586f0fbcec9c127a88b284f5
Jatin Bendale <166155605+bendalejatin@users.noreply.github.com>
2024-11-30 18:53:48 +0530

    Updated practical19.py

--- Python/practical19.py
+++ Python/practical19.py
@@ -1,9 +1,10 @@
-#Name: Jatin Bendale
-#Enrollment Number: 202203103510038
+#Practical : 19-  Write a python program to read the content of a file and return the number of 
+# words in a file to the user.
 
 #Write a python program to read the content of a file and return the number of words in a file to the user.
 total_words=0
 
+#Create a text file named practical19.txt to read.
 file= open(r"/home/A202203103510038/Documents/python/labpracticals/practical19.txt","r")
 
 data= file.read()
