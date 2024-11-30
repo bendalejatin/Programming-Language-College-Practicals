da5650ba255f85029603467983079d43d1432d4b
Jatin Bendale <166155605+bendalejatin@users.noreply.github.com>
2024-11-30 18:59:49 +0530

    Updated practical20.py

--- Python/practical20.py
+++ Python/practical20.py
@@ -1,8 +1,10 @@
-#Name: Jatin Bendale
-#Enrollment Number: 202203103510038
+#Practical : 20- Write a python program to copy the content of one file to another file. 
 
 #Write a python program to copy the content of one file to another file.
+
+#Create a text file named practical19.txt to read.
 file1= open(r"/home/A202203103510038/Documents/python/labpracticals/practical19.txt","r")
+#Create a text file named practical20.txt to write.
 file2= open(r"/home/A202203103510038/Documents/python/labpracticals/practical20.txt","w")
 
 for line in file1:
