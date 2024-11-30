4c98b057d7af99b0b7fe0aff2c753d4115578f81
Jatin Bendale <166155605+bendalejatin@users.noreply.github.com>
2024-11-30 19:06:42 +0530

    Updated practical22.py

--- Python/practical22.py
+++ Python/practical22.py
@@ -1,6 +1,10 @@
-from practical22 import student
+#Practical : 22- Write a python program that import different packages.
+
+# import the student class defined in the check_practical22.py file 
+from check_practical22 import student
 student_name = input("Enter student name:")
 student_branch = input("Enter student branch:")
 
 std = student(student_name,student_branch)
 std.get_data()
+
