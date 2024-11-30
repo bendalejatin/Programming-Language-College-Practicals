d1a73c2d6b39ff2b06e0f496717ffb57d3dc6f2c
Jatin Bendale <166155605+bendalejatin@users.noreply.github.com>
2024-11-30 19:19:39 +0530

    Updated practical1.java

--- Java/practical1.java
+++ Java/practical1.java
@@ -1,1 +1,9 @@
+// PRACTICAL-1: Write a Program that displays Welcome to AMTICS.
 
+// CODE:
+
+public class practical1 {
+    public static void main(String[] args){
+        System.out.println("Welcome to AMTICS.");
+    }
+}
