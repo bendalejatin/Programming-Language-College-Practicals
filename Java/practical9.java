60ea03885eb861f090dceb4eea8c9326c0bceea6
Jatin Bendale <166155605+bendalejatin@users.noreply.github.com>
2024-12-02 15:18:33 +0530

    Updated practical9.java

--- Java/practical9.java
+++ Java/practical9.java
@@ -1,12 +1,7 @@
 /*  PRACTICAL-9:Write a program that generate 6*6 two-dimensional matrix, filled
                 with 0’s and 1’s , display the matrix, check every raw and column
                 have an odd number’s of 1’s.
-
-Name: Jatin Bendale
-Enrollment No.: 202203103510038
-Branch: B.Tech CSE 
 */
-
 // CODE:
 
 import java.util.Scanner;
